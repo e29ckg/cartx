540ad3b12b3d4fd13f53247ae8c064183acab4b3
e29ckg <e29ckg@gmail.com>
2018-12-25 16:30:20 +0700

    m

--- migrations/m181225_090609_report_m.php
+++ migrations/m181225_090609_report_m.php
@@ -12,7 +12,17 @@ class m181225_090609_report_m extends Migration
      */
     public function safeUp()
     {
-
+        $tableOptions = null;
+ 
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        $this->createTable('report_m', [
+            'id' => $this->primaryKey(),
+            'month' => $this->string(),
+            'detail' => $this->string(),
+            'create_at' => $this->dateTime(),
+        ], $tableOptions); 
     }
 
     /**
@@ -21,7 +31,7 @@ class m181225_090609_report_m extends Migration
     public function safeDown()
     {
         echo "m181225_090609_report_m cannot be reverted.\n";
-
+        $this->dropTable('report_m');
         return false;
     }
 
